bf59fa4d0131fae6cd34fe2d1f8fc673805e678b
DaveCaraway <dave@fogmine.com>
2014-09-24 14:36:08 -0400

    Have basic resource tests for Event, Source and LinkRelation resources

--- app/mod_api/controllers.py
+++ app/mod_api/controllers.py
@@ -1,20 +1,37 @@
 # Import flask dependencies
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 from app import app
+from dougrain import Builder
 from flask.ext import restful
 
 # Define the blueprint: 'api', set its url prefix: app.url/api
 mod_api = Blueprint('api', __name__, url_prefix='/api')
 api = restful.Api(mod_api)
 
+class LinkRelations(restful.Resource):
+	"""Link relations for resources of the API"""
+
+	def get(self):
+		return {'hello':'world'}
+
 class Events(restful.Resource):
+	"""Events that are happening"""
+
 	def get(self, complete=False, q='', start=1, limit=20, order='asc'):
 		""" Returns a collection of events matching specified criteria """
-		return {'hello': 'world'}
+		response = Builder("/api/events")
+		response.add_curie('r', "/api/rels/{rel}")
+
+		return response.as_object() 
 
 class Sources(restful.Resource):
+	"""Sources of events"""
+
 	def get(self):
-		return {'hey': 'test'}
+		""" Returns a collection of sources matching specified criteria """
+		response = Builder('/api/sources')
+		return response.as_object() 
 
 api.add_resource(Events, '/events')
 api.add_resource(Sources, '/sources')
+api.add_resource(LinkRelations, '/rels')
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,12 @@
-import unittest, sure, logging, tempfile, os
+import unittest, sure, logging, tempfile, os, json
 import app as vitals
+from dougrain import Document
 
 logger = logging.getLogger(__name__)
 
 class EventTest(unittest.TestCase):
+	"""Tests of api 'Event' resource"""
+
 	def setUp(self):
 		"""Construct temporary database and test client for testing routing and responses"""
 		self.db_fd, self.db_path = tempfile.mkstemp()
@@ -17,9 +20,67 @@ class EventTest(unittest.TestCase):
 		os.close(self.db_fd)
 		os.unlink(self.db_path)
 
-	def test_get_events(self):
+	def test_empty_events(self):
 		"""
-		Get all members of Event collection and verify that it's not None
+		Get all members of Event collection and verify that it's an empty data set
 		"""
-		rv = self.test_client.get('/api/events')
-		(rv).should_not.be.equal(None)
+		resp = self.test_client.get('/api/events')
+		doc = hal_loads(resp.data)
+
+		(resp.status_code).should.equal(200)
+		(doc.links['self'].url()).should.equal('/api/events')
+
+class SourceTest(unittest.TestCase):
+	"""Test of API 'Source' resource"""
+
+	def setUp(self):
+		"""Construct temporary database and test client for testing routing and responses"""
+		self.db_fd, self.db_path = tempfile.mkstemp()
+		vitals.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % self.db_path
+		vitals.app.config['TESTING'] = True
+		self.test_client = vitals.app.test_client()
+		vitals.db.create_all()
+
+	def tearDown(self):
+		"""Removes temporary database at end of each test"""
+		os.close(self.db_fd)
+		os.unlink(self.db_path)
+
+	def test_empty_sources(self):
+		"""
+		Get all members of Sources collection and verify that it's an empty data set
+		"""
+		resp = self.test_client.get('/api/sources')
+		doc = hal_loads(resp.data)
+
+		(resp.status_code).should.equal(200)
+		(doc.links['self'].url()).should.equal('/api/sources')
+
+class LinkRelationTest(unittest.TestCase):
+	"""Test of API 'LinkRelation' resource"""
+
+	def setUp(self):
+		"""Construct temporary database and test client for testing routing and responses"""
+		self.db_fd, self.db_path = tempfile.mkstemp()
+		vitals.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % self.db_path
+		vitals.app.config['TESTING'] = True
+		self.test_client = vitals.app.test_client()
+		vitals.db.create_all()
+
+	def tearDown(self):
+		"""Removes temporary database at end of each test"""
+		os.close(self.db_fd)
+		os.unlink(self.db_path)
+		
+	def test_empty_link_relations(self):
+		"""
+		Get all members of Link Relations collection and verify that it's an empty data set
+		"""
+		resp = self.test_client.get('/api/rels')
+		doc = hal_loads(resp.data)
+
+		(resp.status_code).should.equal(200)
+
+def hal_loads(resp_str):
+	"""Helper function that converts a string into a HAL object"""	
+	return Document.from_object(json.loads(resp_str))
