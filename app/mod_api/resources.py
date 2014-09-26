d865882b455bfc5adf46a4818fc108991bccf625
DaveCaraway <dave@fogmine.com>
2014-09-26 09:16:10 -0400

    migrated the rels back to the resources module

--- app/mod_api/resources.py
+++ app/mod_api/resources.py
@@ -78,7 +78,53 @@ class SourcesList(Resource):
 		response = Builder('/api/sources').add_curie('r', "/api/rels/{rel}")
 		return response.as_object() 
 
+RELS = {
+	"event":
+{
+    "$schema": "http://json-schema.org/schema#",
+
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "email": {"type": "string"}
+    },
+    "required": ["email"]
+},
+
+"source":
+{
+    "$schema": "http://json-schema.org/schema#",
+
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "email": {"type": "string"}
+    },
+    "required": ["email"]
+}
+}
+
+class LinkRelationsList(Resource):
+	"""Link relations for resources of the API"""
+
+	def get(self):
+		"""Gets all link relations"""
+		return RELS
+
+class LinkRelations(Resource):
+	"""Individual link relations"""
+
+	def get(self, rel_id):
+		schema = RELS.get(rel_id)
+
+		if schema is None:
+			abort(404, message="Rel {} doesn't exist".format(rel_id))
+		else:
+			return RELS[rel_id]
+
 api.add_resource(EventsList, '/events', endpoint = 'events')
 api.add_resource(SourcesList, '/sources', endpoint = 'sources')
 api.add_resource(Endpoints, '/', endpoint="endpoints")
+api.add_resource(LinkRelationsList, '/rels/')
+api.add_resource(LinkRelations, '/rels/<string:rel_id>')
 
