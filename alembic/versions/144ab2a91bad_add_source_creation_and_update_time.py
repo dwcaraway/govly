d272a32e492ea1ac00ec6554bee43714dda7c5bd
DaveCaraway <dave@fogmine.com>
2014-10-06 10:16:30 -0400

    added alembic migrations for database

---
+++ alembic/versions/144ab2a91bad_add_source_creation_and_update_time.py
@@ -0,0 +1,26 @@
+"""add source creation and update time
+
+Revision ID: 144ab2a91bad
+Revises: None
+Create Date: 2014-10-06 10:06:23.961087
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '144ab2a91bad'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('source', sa.Column('created_on', sa.DateTime))
+    op.add_column('source', sa.Column('updated_on', sa.DateTime))
+    pass
+
+
+def downgrade():
+    op.drop_column('source', 'created_on')
+    op.drop_column('source', 'updated_on')
+    pass
