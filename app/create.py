2401f982924c0e0fcf184259210bcd1a3ef85ca3
DaveCaraway <dave@fogmine.com>
2014-11-08 10:33:30 -0500

    revising the data models for scraping and handling a wider variety of data

--- app/create.py
+++
@@ -1,5 +0,0 @@
-from app.new_model import metadata
-from sqlalchemy import create_engine
-
-engine = create_engine('postgresql://vitals:vitals@localhost:5432/vitals')
-metadata.create_all(engine)
--- app/model.py
+++ app/model.py
@@ -21,6 +21,52 @@ def get_string_repr(obj):
 
     return json.dumps(new_obj)
 
+class Person(db.Model):
+
+    __tablename__ = 'people'
+
+    id = db.Column(db.Integer, primary_key=True)
+    birthDate = db.Column(db.Date)
+    givenName = db.Column(db.String)
+    familyName = db.Column(db.String)
+    telephone = db.Column(db.String)
+
+    created_on = db.Column(db.DateTime, default=db.func.now())
+    updated_on = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+
+
+class Organization(db.Model):
+
+    __tablename__= 'organizations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    duns = db.Column(db.String)
+    legalName = db.Column(db.String)
+    logo = db.Column(db.String)
+    naics = db.Column(db.String)
+    cage = db.Column(db.String)
+    taxID = db.Column(db.String)
+    description = db.Column(db.Text)
+
+    addressCountry = db.Column(db.String)
+    addressLocality = db.Column(db.String)
+    addressRegion = db.Column(db.String)
+    postOfficeBoxNumber = db.Column(db.String)
+    postalCode = db.Column(db.String)
+    streetAddress = db.Column(db.String)
+    lat = db.Column(db.String)
+    lon = db.Column(db.String)
+
+    created_on = db.Column(db.DateTime, default=db.func.now())
+    updated_on = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+
+    employees = db.relationship("Person", secondary="employment", backref="employers")
+
+db.Table('employment', db.Model.metadata,
+                db.Column('employer_id', None, db.ForeignKey('organizations.id')),
+                db.Column('employee_id', None, db.ForeignKey('people.id'))
+)
+
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String(80))
@@ -76,23 +122,20 @@ class Business(db.Model):
     """A data model for a business"""
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    phone = db.Column(db.String(25))
-    website = db.Column(db.String(400))
-    facebook = db.Column(db.String(400))
-    twitter = db.Column(db.String(400))
-    linkedin = db.Column(db.String(400))
-    logo = db.Column(db.String(400))
-    category = db.Column(db.String(200))
-    description = db.Column(db.String(2000))
-    email = db.Column(db.String(400))
+    name = db.Column(db.String)
+    phone = db.Column(db.String)
+    website = db.Column(db.String)
+    logo = db.Column(db.String)
+    category = db.Column(db.String)
+    description = db.Column(db.String)
+    email = db.Column(db.String)
 
-    raw_address = db.Column(db.String(300))
-    address1 = db.Column(db.String(300))
-    address2 = db.Column(db.String(300))
-    city = db.Column(db.String(100))
-    state = db.Column(db.String(70))
-    zip = db.Column(db.String(15))
+    raw_address = db.Column(db.String)
+    address1 = db.Column(db.String)
+    address2 = db.Column(db.String)
+    city = db.Column(db.String)
+    state = db.Column(db.String)
+    zip = db.Column(db.String)
     latitude = db.Column(db.Float)
     longitude =db.Column(db.Float)
 
@@ -126,3 +169,40 @@ class User(db.Model):
 
     def __repr__(self):
         return get_string_repr(self)
+
+class Link(db.Model):
+    __tablename__ = 'links'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rel = db.Column(db.String)
+    href = db.Column(db.String)
+
+    created_on = db.Column(db.DateTime, default=db.func.now())
+    updated_on = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    url = db.Column(db.String)
+
+    created_on = db.Column(db.DateTime, default=db.func.now())
+    updated_on = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+
+class ContactPoint(db.Model):
+    __tablename__= 'contact_points'
+
+    id = db.Column(db.Integer, primary_key=True)
+    operating_hours = db.relationship("OperatingHours", backref="contact")
+    email = db.Column(db.String)
+    faxNumber = db.Column(db.String)
+    telephone = db.Column(db.String)
+
+class OperatingHours(db.Model):
+    __tablename__= 'operating_hours'
+    id = db.Column(db.Integer, primary_key=True)
+    closes = db.Column(db.String)
+    opens = db.Column(db.String)
+    dayOfWeek = db.Column(db.String)
+    contact_id = db.Column(db.Integer, db.ForeignKey('contact_points.id'))
